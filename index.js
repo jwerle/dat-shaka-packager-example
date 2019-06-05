56a9ccf9c78555d1673c8c38708927f950a37a85
Joseph Werle <joseph.werle@gmail.com>
2019-06-05 11:23:59 -0400

    refactor(index.js): Handle errors and use 'hypercore-protocol' directly

--- index.js
+++ index.js
@@ -1,11 +1,92 @@
 const hyperdrive = require('hyperdrive')
+const protocol = require('hypercore-protocol')
 const storage = require('dat-storage')
+const crypto = require('hypercore-crypto')
+const rimraf = require('rimraf')
 const swarm = require('discovery-swarm')
 const path = require('path')
 const ram = require('random-access-memory')
 
+const TIMEOUT = 10000
 const dirname = './input'
-const key = '66dea7bef181330e5454c6a4cb7b848d838631ebb9e1bcd73b1bafb78bf51ae2'
+const key = '3546e77a133f01721058fb96cd9034d2cbf1e665eb040455e6eec614a8206bb7'
+
+function connect(key, dirname, callback) {
+  const opts = require('dat-swarm-defaults')({ hash: false, stream: onstream })
+  const input = hyperdrive(storage(dirname), { latest: true })
+  const discovery = swarm(opts)
+  const discoveryKey = crypto.discoveryKey(Buffer.from(key, 'hex'))
+
+  let connected = false
+  let retries = 3
+
+  input.ready(onready)
+
+  discovery.on('error', onerror)
+  input.on('error', onerror)
+
+  function onerror(err) {
+    connected = false
+    if (0 === --retries) {
+      callback(err)
+    }
+  }
+
+  function onready() {
+    discovery.join(discoveryKey)
+  }
+
+  function onstream() {
+    const stream = protocol({ live: true, userData: input.key })
+    stream.feed(Buffer.from(key, 'hex'))
+    stream.once('handshake', onhandshake)
+    stream.once('error', callback)
+    return stream
+
+    function onhandshake() {
+      if (connected) {
+        return stream.finalize()
+      }
+
+      connected = true
+      const key = stream.remoteUserData.toString('hex')
+      const output = path.join(`./tmp/${key}`)
+      const response = hyperdrive(storage(output), key, { latest: true })
+
+      let timeout = setTimeout(ontimeout, TIMEOUT)
+
+      response.replicate({ stream, live: true })
+      input.replicate({ stream, live: true })
+
+      response.on('error', callback)
+      response.on('sync', onsync)
+      response.on('update', () => {
+        clearTimeout(timeout)
+        timeout = setTimeout(ontimeout, TIMEOUT)
+      })
+
+      function onsync() {
+        clearTimeout(ontimeout)
+        stream.destroy()
+        discovery.close()
+        response.readFile('error.json', (err, buf) => {
+          if (err) {
+            callback(null, response)
+          } else {
+            callback(Object.assign(new Error(), JSON.parse(buf)), null)
+          }
+        })
+      }
+
+      function ontimeout() {
+        retries--
+        connected = false
+        stream.destroy()
+        rimraf(output, (err) => err && console.error(err))
+      }
+    }
+  }
+}
 
 connect(key, dirname, (err, res) => {
   if (err) {
@@ -24,45 +105,3 @@ connect(key, dirname, (err, res) => {
     process.nextTick(process.exit, 0)
   })
 })
-
-function connect(key, dirname, callback) {
-  const opts = require('dat-swarm-defaults')({ hash: false, stream: onstream })
-  const input = hyperdrive(storage(dirname), { latest: true })
-  const request = hyperdrive(ram, key, { latest: true })
-  const discovery = swarm(opts)
-
-  input.ready(() => request.ready(onready))
-
-  discovery.on('error', callback)
-  request.on('error', callback)
-  input.on('error', callback)
-
-  function onready() {
-    discovery.join(request.discoveryKey)
-  }
-
-  function onstream() {
-    const stream = request.replicate({ live: true, userData: input.key })
-    stream.on('handshake', onhandshake)
-    stream.on('error', callback)
-    return stream
-
-    function onhandshake() {
-      const key = stream.remoteUserData.toString('hex')
-      const output = path.join(`./tmp/${key}`)
-      const response = hyperdrive(storage(output), key, { latest: true })
-
-      response.replicate({ stream, live: true })
-      input.replicate({ stream, live: true })
-
-      response.on('error', callback)
-      response.on('sync', onsync)
-
-      function onsync() {
-        stream.destroy()
-        discovery.close()
-        callback(null, response)
-      }
-    }
-  }
-}
